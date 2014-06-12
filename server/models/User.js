500c9cd98c0553dd253eb25d709bad3cb7986f8b
Kevin Romano <kevrom@gmail.com>
2014-06-12 16:56:25 -0500

    building out user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,43 @@
+crypto = require('crypto');
+
 module.exports = function(sequelize, DataTypes) {
 	var User = sequelize.define('User', {
 		username = DataTypes.STRING,
-		password = DataTypes.STRING
+		hashed_password = DataTypes.STRING,
+		name = DataTypes.STRING,
+		email = DataTypes.STRING,
+		provider = DataTypes.STRING,
+		salt = DataTypes.STRING,
+		roles = DataTypes.STRING,
+		isActive = DataTypes.BOOLEAN
 	}, {
-		classMethods: {}
+		_password: '',
+		classMethods: {},
+		instanceMethods: {
+			authenticate: function(plainText) {
+				return this.encryptPassword(plainText) === this.hashed_password;
+			},
+			encryptPassword: function(password) {
+				return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+			},
+			getPassword: function() {
+				return this._password;
+			},
+			makeSalt: function() {
+				var set = '0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ';
+				var salt = '';
+				for (var i=0; i<10; i++) {
+					var p = Math.floor(Math.random() * set.length);
+					salt += set[p];
+				}
+				return salt;
+			},
+			setPassword: function(password) {
+				this._password = password;
+				this.salt = this.makeSalt();
+				this.hashed_password = this.encryptPassword(password);
+			}
+		}
 	});
 
 	return User;

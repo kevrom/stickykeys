aca40cbfb3e0e1b03a3652baa10964af5e2d2b75
Kevin Romano <kevrom@gmail.com>
2014-07-21 16:56:44 -0500

    building out my gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -7,5 +7,30 @@ var nodemon = require('gulp-nodemon');
 var sass = require('gulp-ruby-sass');
 var notify = require('gulp-notify');
 
+var path = require('path');
+var config = require('./server/config/config');
+
+gulp.task('styles', function() {
+	return gulp.src('public/scss/main.scss')
+		.pipe(sass({
+			style: 'expanded'
+		}))
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('jshint', function() {
+	return gulp.src(['app.js', 'gulpfile.js', './server/**/*.js', './client/js/**/*.js'])
+		.pipe(jshint());
+});
+
 gulp.task('default', function() {
+	nodemon({
+		script: 'app.js',
+		ext: 'html js',
+	})
+	.on('change', ['jshint'])
+	.on('restart', function() {
+		console.log('Server restarted.');
+		notify('Development server restarted.');
+	});
 });

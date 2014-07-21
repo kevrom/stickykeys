e90756223e8eaabe2c77368b3c8e1ece23b63abe
Kevin Romano <kevrom@gmail.com>
2014-07-21 01:11:53 -0500

    added gulp packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
+var sass = require('gulp-ruby-sass');
+var notify = require('gulp-notify');
 
 gulp.task('default', function() {
-	// nothing
 });

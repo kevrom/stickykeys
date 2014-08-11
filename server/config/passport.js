9fc929edda99c274296efa64f76d7b916e6c28c3
Kevin Romano <kevrom@gmail.com>
2014-08-11 03:44:16 -0500

    changing mongoose functions to sequelize

--- server/config/passport.js
+++ server/config/passport.js
@@ -30,7 +30,7 @@ module.exports = function (app, passport) {
 		},
 		function(email, password, done) {
 
-			User.findOne( { email: email } , function (err, user) {
+			User.find( { email: email } , function (err, user) {
 
 				if (err) {
 					return done(err);

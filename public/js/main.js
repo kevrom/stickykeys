b57c72f22b3f8ed1d6d7940ec1a2fa96759512a3
Kevin Romano <kevrom@gmail.com>
2014-09-16 21:00:26 -0500

    layout finalized, added like button, consistency, lots of shit

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
-/* global io */
-
-//var socket = io();
-
+(function(d, s, id) {
+	/* global document */
+	'use strict';
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) { return; }
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=358579180960242&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 

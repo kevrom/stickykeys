a6857a344f6084fd82c2e0eb576a86f82a900223
Kevin Romano <kevrom@gmail.com>
2014-09-21 02:30:07 -0500

    added an email handler class for templating, support tickets send emails

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,6 +2,7 @@
 
 var IndexController = {};
 var config = require('../config/config');
+var Emailer = require(config.root + '/server/utils/emailer');
 var Ticket = require(config.root + '/server/models').Ticket;
 
 IndexController.index = function(req, res) {
@@ -11,6 +12,23 @@ IndexController.index = function(req, res) {
 };
 
 IndexController.ticket = function(req, res) {
+	var customerEmailOptions = {
+		template: 'customer',
+		subject: 'Sticky Keys Appointment',
+		to: {
+			name: req.body.fullName,
+			email: req.body.email
+		}
+	};
+	var supportEmailOptions = {
+		template: 'ticket',
+		subject: 'Appointment Request - ' + req.body.fullName,
+		to: {
+			name: 'Sticky Keys Support',
+			email: 'support@sticky-keys.com'
+		}
+	};
+	var emailData = req.body;
 	console.log(req.body);
 
 	//Ticket.create({
@@ -20,6 +38,18 @@ IndexController.ticket = function(req, res) {
 		//allow_email: req.body.allowEmail,
 		//problem: req.body.problem
 	//});
+
+	if (req.body.allowEmail) {
+		var customerEmail = new Emailer(customerEmailOptions, emailData);
+		customerEmail.send(function(err, info) {
+			if (err) { console.error(err); }
+		});
+	}
+
+	var ticketEmail = new Emailer(supportEmailOptions, emailData);
+	ticketEmail.send(function(err, info) {
+		if (err) { console.error(err); }
+	});
 
 	req.flash('success', 'Thanks for contacting us.  We will get back with you as soon as possible.');
 	res.redirect('/');
